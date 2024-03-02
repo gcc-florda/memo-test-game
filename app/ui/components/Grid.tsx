66b0dd2dfc6b27fbb5dc3e68a06b28c354aa729b
Francisco Orquera Lorda <forqueral@fi.uba.ar>
2024-03-02 14:28:29 -0300

    Card component flipping animation

--- app/ui/components/Grid.tsx
+++ app/ui/components/Grid.tsx
@@ -2,18 +2,20 @@
 
 import * as React from 'react';
 import { Grid } from '@mui/material';
-import { MemoCard } from './Card';
+import { MemoCard } from './MemoCard';
 
 export function GameGrid() {
     return (
-        <div>
-            <Grid container spacing={2}>
-                {Array.from(Array(6)).map((_, index) => (
-                    <Grid item xs={2} sm={4} key={index}>
-                        <MemoCard />
-                    </Grid>
-                ))}
-            </Grid>
+        <div className="min-h-screen flex items-center justify-center px-4"> {/* Add padding to both sides */}
+            <div className="w-full max-w-screen-lg"> {/* Add padding to the left */}
+                <Grid container spacing={2}>
+                    {Array.from(Array(6)).map((_, index) => (
+                        <Grid item xs={2} sm={4} key={index}>
+                            <MemoCard />
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
         </div>
     );
 }
---
+++ app/ui/components/MemoCard.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import styled from '@mui/system/styled';
+import { useState } from 'react';
+
+const FrontCard = styled('div')({
+    position: 'absolute',
+    backgroundColor: '#ff8a80',
+    height: 300,
+    width: 250,
+    display: 'flex',
+    borderRadius: 8,
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+    backfaceVisibility: 'hidden',
+    '&:hover': {
+        backgroundColor: '#e57373',
+    },
+});
+
+const BackCard = styled('div')({
+    position: 'absolute',
+    backgroundColor: '#ea80fc',
+    height: 300,
+    width: 250,
+    display: 'flex',
+    borderRadius: 8,
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+    backfaceVisibility: 'hidden',
+    transform: 'rotateY(180deg)',
+    '&:hover': {
+        backgroundColor: '#ba68c8',
+    },
+});
+
+const Card = styled('div')({
+    height: 300,
+    width: 250,
+    display: 'flex',
+    borderRadius: 8,
+    cursor: 'pointer',
+    transition: 'transform 0.6s',
+    transformStyle: 'preserve-3d',
+    '&:hover': {
+        backgroundColor: '#0d47a1',
+    },
+});
+
+export function MemoCard() {
+    const [isFlipped, setIsFlipped] = useState(true);
+
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+    return (
+        <Card style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)' }}>
+            <FrontCard onClick={handleClick}>
+                Front
+            </FrontCard>
+            <BackCard onClick={handleClick}>
+                Back
+            </BackCard>
+        </Card>
+    );
+}
