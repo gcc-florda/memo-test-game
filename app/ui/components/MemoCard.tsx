771c8bf70a0c34407d9b01d8bf9f97bc2cc4869b
Francisco Orquera Lorda <forqueral@fi.uba.ar>
2024-03-03 20:49:54 -0300

    started with the cards background img

--- app/ui/components/MemoCard.tsx
+++ app/ui/components/MemoCard.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import styled from '@mui/system/styled';
-import { useState } from 'react';
 
 const FrontCard = styled('div')({
     position: 'absolute',
-    backgroundColor: '#ff8a80',
     height: 300,
     width: 250,
     display: 'flex',
@@ -13,14 +11,15 @@ const FrontCard = styled('div')({
     cursor: 'pointer',
     transition: 'background-color 0.3s ease',
     backfaceVisibility: 'hidden',
+    border: '1px solid black', // Add black border
     '&:hover': {
         backgroundColor: '#e57373',
-    },
+    }
 });
 
 const BackCard = styled('div')({
     position: 'absolute',
-    backgroundColor: '#ea80fc',
+    // backgroundColor: '#ea80fc',
     height: 300,
     width: 250,
     display: 'flex',
@@ -29,6 +28,7 @@ const BackCard = styled('div')({
     transition: 'background-color 0.3s ease',
     backfaceVisibility: 'hidden',
     transform: 'rotateY(180deg)',
+    border: '1px solid black', // Add black border
     '&:hover': {
         backgroundColor: '#ba68c8',
     },
@@ -42,20 +42,17 @@ const Card = styled('div')({
     cursor: 'pointer',
     transition: 'transform 0.6s',
     transformStyle: 'preserve-3d',
-    '&:hover': {
-        backgroundColor: '#0d47a1',
-    },
 });
 
 export function MemoCard({ id, isFlipped }: { id: number, isFlipped: boolean }) {
     return (
         <Card style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)' }}>
-            <FrontCard>
+            <FrontCard className={`bg-[url(/animals/1.png)]`}>
                 Front {id}
             </FrontCard>
-            <BackCard>
+            <BackCard className='bg-gradient-to-r from-sky-500 to-indigo-500'>
                 Back {id}
             </BackCard>
-        </Card>
+        </Card >
     );
 }
