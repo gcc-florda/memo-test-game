42ae30de3bcb9f2669302a87503cadbd41da0095
Francisco Orquera Lorda <forqueral@fi.uba.ar>
2024-03-08 19:18:15 -0300

    commented get session queries

--- frontend/app/home/game/[id]/page.tsx
+++ frontend/app/home/game/[id]/page.tsx
@@ -7,23 +7,23 @@ export default async function Page({ params }: { params: { id: string } }) {
     const memoTestData = await getMemoTest(params.id);
     const memoTestImages = JSON.parse(memoTestData["get_memo_test"].images);
 
-    const gameSessionData = await getGameSession(params.id);
-    let gameSession = gameSessionData["get_game_session"];
-    let retries;
-    let state;
+    // const gameSessionData = await getGameSession(params.id);
+    // let gameSession = gameSessionData["get_game_session"];
+    // let retries;
+    // let state;
 
-    if (gameSession == null) {
-        const startGameSessionData = await createGameSession(params.id, params.id, 0, 0, 'STARTED');
-        gameSession = startGameSessionData["create_game_session"]
-    }
-    retries = gameSession.retries;
-    state = gameSession.state;
+    // if (gameSession == null) {
+    //     const startGameSessionData = await createGameSession(params.id, params.id, 0, 0, 'STARTED');
+    //     gameSession = startGameSessionData["create_game_session"]
+    // }
+    // retries = gameSession.retries;
+    // state = gameSession.state;
 
-    console.log(retries, state);
+    // console.log(retries, state);
 
     return (
         <main>
-            <MemoTest id={params.id} images={memoTestImages} number_retries={retries} state={state} />
+            <MemoTest id={params.id} images={memoTestImages} />
         </main>
     );
 }
